1bbe358ae700c5c0f235f40f0cc707b8543f0769
Gandharthus <72447580+Gandharthus@users.noreply.github.com>
2021-07-27 20:06:06 +0100

    commit on 'MAC'

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = {'xlsx', 'csv'}
-CHOICE_CODES={6108:'casa', 7102:'meknes', 5110:'rabat'}
+CHOICE_CODES={6108:'casa', 7102:'meknes', 5660:'rabat'}
 ENSAM_CODES=dict((v, k) for k, v in CHOICE_CODES.items())
 SECRET_KEY = '002a5938ad85bac7145ce707c8404ce0cf4a94c4ee705734'
